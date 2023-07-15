24e50013b45d923cb2f17e22a00591ebfd5333b7
unknown <neilmagic25@gmail.com>
2023-07-15 12:41:13 -0400

    Adding and deleting code

--- Exercise Files/Ch04/04_02/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_02/start/react-app/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { useRef  } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const submit = (e) => {
+    e.preventDefault();
+
+  };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={submit}>
+  <input type="text" placeholder="color title..." /> 
+  <input type="color" />
+  <button>Add</button>
+    </form>
+
+    
+    
+  
   );
 }
 
